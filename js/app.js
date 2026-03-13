329bd36d459789374715619c846d63cfa6e189bd
raffone85 <raffaelefontana85@gmail.com>
2026-03-13 10:38:49 +0100

    Fetch and display match data from JSON

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-console.log("App caricata");
+fetch("data/partite.json")
+.then(response => response.json())
+.then(data => {
+
+const container = document.querySelector(".container")
+
+data.partite.forEach(p => {
+
+const card = document.createElement("div")
+
+card.className = "card"
+
+card.innerHTML = `
+<h3>${p.home} vs ${p.away}</h3>
+<p>Tiri: ${p.tiri_home} - ${p.tiri_away}</p>
+<p>Falli: ${p.falli_home} - ${p.falli_away}</p>
+`
+
+container.appendChild(card)
+
+})
+
+})
